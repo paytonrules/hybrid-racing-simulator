e86febd6a98ef3efc79908012d31147a217d5f80
Eric Smith <paytonrules@gmail.com>
2026-03-05 23:13:37 -0600

    Begin a NES style game style, for now

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
 use dioxus::prelude::*;
 
+static CSS: Asset = asset!("/assets/main.css");
+
 #[derive(Routable, Clone)]
 enum Route {
     #[route("/")]
     Home {},
-    #[route("/teams")]
-    Teams {},
-    #[route("/schedule")]
-    Schedule {},
-    #[route("/settings")]
-    Settings {},
 }
 
 fn main() {
@@ -26,63 +22,15 @@ fn App() -> Element {
 #[component]
 fn Home() -> Element {
     rsx! {
-        div { class: "page",
-            h1 { "Sports League" }
-            p { "Welcome to the game" }
-            Navigation {}
-        }
-    }
-}
-
-#[component]
-fn Teams() -> Element {
-    rsx! {
-        div { class: "page",
-            h1 { "Teams" }
-            ul {
-                li { "Team A" }
-                li { "Team B" }
-                li { "Team C" }
+        document::Stylesheet { href: CSS }
+        div { class: "container",
+            header { "Hybrid Race Simulator" }
+            div { class: "left nes-container",
+                p { "left side" }
             }
-            Navigation {}
-        }
-    }
-}
-
-#[component]
-fn Schedule() -> Element {
-    rsx! {
-        div { class: "page",
-            h1 { "Schedule" }
-            ul {
-                li { "Game 1 - Monday" }
-                li { "Game 2 - Wednesday" }
-                li { "Game 3 - Friday" }
+            div { class: "right nes-container",
+                div { "The money" }
             }
-            Navigation {}
-        }
-    }
-}
-
-#[component]
-fn Settings() -> Element {
-    rsx! {
-        div { class: "page",
-            h1 { "Settings" }
-            p { "Coming soon..." }
-            Navigation {}
-        }
-    }
-}
-
-#[component]
-fn Navigation() -> Element {
-    rsx! {
-        nav { class: "nav",
-            Link { to: Route::Home {}, "Home" }
-            Link { to: Route::Teams {}, "Teams" }
-            Link { to: Route::Schedule {}, "Schedule" }
-            Link { to: Route::Settings {}, "Settings" }
         }
     }
 }
