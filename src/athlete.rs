04795203335f8be0478245fce868869db531665f
Eric Smith <paytonrules@gmail.com>
2026-03-22 08:52:08 -0500

    Start storing races as a list
    I kinda want an in memory database TBH. Store stuff with foreign keys. But that's overkill.
    
    Anyway now races get pushed onto a list.

--- src/athlete.rs
+++ src/athlete.rs
@@ -5,16 +5,21 @@ pub struct Athlete {
     pub name: String,
     pub fatigue: i8,
     pub fitness: u8,
-    pub pr: u16,
+    pub races: Vec<u16>,
 }
 
 impl Athlete {
     pub fn pr(&self) -> String {
-        let hours = self.pr / 3600;
-        let minutes = (self.pr % 3600) / 60;
-        let seconds = self.pr % 60;
+        let pr = self.races.iter().min().unwrap_or_else(|| &0);
+        let hours = pr / 3600;
+        let minutes = (pr % 3600) / 60;
+        let seconds = pr % 60;
 
         format!("{hours}:{minutes:02}:{seconds:02}")
+    }
+
+    pub fn log_race(&mut self, time: u16) {
+        self.races.push(time);
     }
 }
 
@@ -25,10 +30,23 @@ mod tests {
     #[test]
     fn it_can_format_pr_into_min_seconds() {
         let athlete = Athlete {
-            pr: 5565,
+            races: vec![5565],
             ..Default::default()
         };
 
+        assert_eq!(athlete.pr(), "1:32:45");
+    }
+
+    #[test]
+    fn it_stores_all_race_results() {
+        let mut athlete = Athlete::default();
+
+        assert_eq!(athlete.pr(), "0:00:00");
+
+        athlete.log_race(5565);
+        assert_eq!(athlete.pr(), "1:32:45");
+
+        athlete.log_race(5565 + 1);
         assert_eq!(athlete.pr(), "1:32:45");
     }
 }
